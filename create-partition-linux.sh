e5789a602efb14877088877bdaa4f6f80a2cc4e1
steel-a <64929461+steel-a@users.noreply.github.com>
2020-05-08 18:36:22 -0300

    Togle the partition bootable

--- create-partition-linux.sh
+++ create-partition-linux.sh
@@ -9,6 +9,7 @@ p # primary
 1 # partition number 1
 	# default, start immediately after preceding partition
 	# default, extend partition to end of disk
+a # togle bootable
 p # print partition table
 w # write the partition table
 EOF
