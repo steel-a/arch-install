3880c5a826e2f68c1ef198914d27f6115816be69
steel-a <64929461+steel-a@users.noreply.github.com>
2020-05-20 19:09:49 -0300

    Mount fileserver partition N add to fstab

--- install-fileserver-samba-nextcloud.sh
+++ install-fileserver-samba-nextcloud.sh
@@ -1,11 +1,19 @@
 FILESERVER_PARTITION="sdb1"
 PATH_TO_MOUNT="/mnt/data"
+PARTITION_TYPE="ext4"
 
 SAMBA_USER="mi"
 SAMBA_MOUNT_LABEL="data"
 
 PROJ_PATH="https://raw.githubusercontent.com/steel-a/arch-install/master/"
 
-UUID="$(blkid | grep -m 1 -oP '((?<=${FILESERVER_PARTITION}: UUID=")([a-z0-9-]*))')"
+UUID="$(blkid | grep ${FILESERVER_PARTITION} | grep -m 1 -oP '((?<=: UUID=")([a-z0-9-]*))')"
 
-echo $UUID
+if grep -q $UUID /etc/fstab
+then
+  echo "UUID is already in fstab"
+else
+  echo "${UUID}= ${PATH_TO_MOUNT} ${PARTITION_TYPE} defauts 0 2" >> /etc/fstab
+  mkdir ${PATH_TO_MOUNT}
+  mount /dev/${FILESERVER_PARTITION} ${PATH_TO_MOUNT}
+fi
