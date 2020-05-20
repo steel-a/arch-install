de08d453fb04d0de6f4932cc701a9c97cddf6438
steel-a <64929461+steel-a@users.noreply.github.com>
2020-05-20 13:06:59 -0300

    get UUID

--- install-fileserver-samba-nextcloud.sh
+++ install-fileserver-samba-nextcloud.sh
@@ -1,1 +1,11 @@
+FILESERVER_PARTITION="sdb1"
+PATH_TO_MOUNT="/mnt/data"
 
+SAMBA_USER="mi"
+SAMBA_MOUNT_LABEL="data"
+
+PROJ_PATH="https://raw.githubusercontent.com/steel-a/arch-install/master/"
+
+UUID="$(blkid | grep -m 1 -oP '((?<=${FILESERVER_PARTITION}: UUID=")([a-z0-9-]*))')"
+
+echo $UUID
