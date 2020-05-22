d6061ef13889bab01a9c4b8cce15e780723dac68
steel-a <64929461+steel-a@users.noreply.github.com>
2020-05-22 15:45:02 -0300

    Update 02-set-static-ip.sh

--- 02-set-static-ip.sh
+++ 02-set-static-ip.sh
@@ -1,1 +1,18 @@
-yes | pacman -S openresolv netctl dialog
+IP="192.168.117.5"
+GATEWAY="192.168.117.1"
+DNS="192.168.117.1"
+LAN_INTERFACE="$(ls /sys/class/net | grep -m 1 -oP "en[a-z0-9]*")"
+
+yes | pacman -S openresolv netctl #dialog wpa_supplicant
+
+echo "Description='A basic static ethernet connection'" > /etc/netctl/ethernet-static
+echo "Interface=${LAN_INTERFACE}" >> /etc/netctl/ethernet-static
+echo "Connection=ethernet" >> /etc/netctl/ethernet-static
+echo "IP=static" >> /etc/netctl/ethernet-static
+echo "Address=('${IP}/24')" >> /etc/netctl/ethernet-static
+echo "Gateway='${GATEWAY}'" >> /etc/netctl/ethernet-static
+echo "DNS=('${DNS}')" >> /etc/netctl/ethernet-static
+
+
+systemctl disable dhcpcd
+netctl enable ethernet-static
